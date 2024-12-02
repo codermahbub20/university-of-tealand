a7be78896230029fbd1d9d8ab8dc90f3c7bfe427
codermahbub20 <mkrefat5@gmail.com>
2024-12-02 20:38:12 +0600

    Remove status role isDelated id form the user validation file because we refector the code

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,14 +1,12 @@
 import { z } from 'zod';
 
 const userValidationSchema = z.object({
-  id: z.string(),
   password: z
-    .string()
-    .max(20, { message: 'Password can not be more than 20 characters' }),
-  needsPasswordChange: z.boolean().optional(),
-  role: z.enum(['admin', 'faculty', 'student']),
-  status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-  isDeleted: z.boolean().default(false).optional(),
+    .string({
+      invalid_type_error: 'Password Must Be String',
+    })
+    .max(20, { message: 'Password can not be more than 20 characters' })
+    .optional(),
 });
 
 export const UserValidationSchema = {
