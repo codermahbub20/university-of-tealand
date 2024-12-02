b0109c74832728eb8ccefaece6119dc8e2b337ac
codermahbub20 <mkrefat5@gmail.com>
2024-12-02 23:55:37 +0600

    Global error handller implement

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 
 const app: Application = express();
 import cors from 'cors';
 import { StudentRoutes } from './app/modules/students/student.routes';
 import { userRoutes } from './app/modules/user/user.routes';
+import { error } from 'console';
+import globalErrorHandler from './app/middlewares/globalErrorHandalers';
 
 //  parsers
 app.use(express.json());
@@ -16,5 +18,7 @@ app.use('/api/v1/users', userRoutes);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+app.use(globalErrorHandler);
 
 export default app;
