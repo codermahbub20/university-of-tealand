b1627e3bece3d1fae6409bd97dffa0bd9714d204
codermahbub20 <mkrefat5@gmail.com>
2024-12-07 00:29:52 +0600

    Create academic faculty

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -22,7 +22,7 @@ export const generateStudentId = async (payload: TAcademicSemester) => {
   let currentId = (0).toString(); // by default 00000
 
   const lastStudentId = await findLastStudentId();
-
+  //  Here I check and validate my code ,like  if a user admit 2030 01->( this indicate Autumn Semester ) 0001-> student id, semester and second student admit 2030 02-> summer semester can not the student id start previous student id like 001 + 1 = 002 , its start with 2030 02 0001 {
   const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
   const latStudentYear = lastStudentId?.substring(0, 4);
 
@@ -36,6 +36,8 @@ export const generateStudentId = async (payload: TAcademicSemester) => {
   ) {
     currentId = lastStudentId?.substring(6);
   }
+
+  // } Here I check and validate my code ,like  if a user admit 2030 01->( this indicate Autumn Semester ) 0001-> student id, semester and second student admit 2030 02-> summer semester can not the student id start previous student id like 001 + 1 = 002 , its start with 2030 02 0001
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
