c2274351f919e9e2c969e7b28c8b6edb5c5da52d
codermahbub20 <mkrefat5@gmail.com>
2024-12-09 23:53:25 +0600

    Add something

---
+++ src/app/errors/strict.ts
@@ -0,0 +1,3 @@
+/*
+  Ajke Tarikh 09/12/2024 rat 11:51 min, Semester final exam er pressure er karone github e kisu push korte pari nai , ami chai agamai ek years ami daily at least ekta holw github e commit korbo , thats why ami ajke ei faltu jinish commit kortesi
+*/
