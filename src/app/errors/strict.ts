a7bad080abd249055c955c51953d77a7638abad3
codermahbub20 <mkrefat5@gmail.com>
2024-12-09 23:58:17 +0600

    Fixed error issue

--- src/app/errors/strict.ts
+++ src/app/errors/strict.ts
@@ -2,4 +2,5 @@
   Ajke Tarikh 09/12/2024 rat 11:51 min, Semester final exam er pressure er karone github e kisu push korte pari nai , ami chai agamai ek years ami daily at least ekta holw github e commit korbo , thats why ami ajke ei faltu jinish commit kortesi
   1 ta commit kore shanti lagtase nah arekta kori
   durr arek ta kori ki ace jibone 
+  Oh Interesting bepar cheat chat and meet with Abdur Rakib Bhai
 */
