a700a07a551b783fdf4b52aac33569bfc345a809
codermahbub20 <mkrefat5@gmail.com>
2024-12-10 00:51:45 +0600

    Convert Zod Error

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -8,4 +8,5 @@ export default {
   database_url: process.env.DATABASE_URL,
   default_pass: process.env.DEFAULT_PASS,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  NODE_ENV: process.env.NODE_ENV,
 };
