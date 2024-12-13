4d5badf54bd1fad199c35909fe1ecc8adf552951
codermahbub20 <mkrefat5@gmail.com>
2024-12-13 23:21:20 +0600

    create raw filtering ,sorting and limiting

--- src/app/modules/students/student.service.ts
+++ src/app/modules/students/student.service.ts
@@ -5,17 +5,32 @@ import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 
 const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  //
+
+  const queryObj = { ...query }; // copy
+
+  const studentSearchableFields = ['email', 'name.firstName', 'presentAddress'];
+
   let searchTerm = '';
 
   if (query?.searchTerm) {
     searchTerm = query.searchTerm as string;
   }
 
-  const result = await Student.find({
-    $or: ['email', 'name.firstName', 'presentAddress'].map((field) => ({
+  // Search Query
+  const searchQuery = Student.find({
+    $or: studentSearchableFields.map((field) => ({
       [field]: { $regex: searchTerm, $options: 'i' },
     })),
-  })
+  });
+
+  // filtering
+  const excludedFields = ['searchTerm', 'sort', 'limit'];
+
+  excludedFields.forEach((el) => delete queryObj[el]);
+
+  const filterQuery = searchQuery
+    .find(queryObj)
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
@@ -23,7 +38,26 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
         path: 'academicFaculty',
       },
     });
-  return result;
+
+  let sort = '-createdAt'; // SET DEFAULT VALUE
+
+  if (query.sort) {
+    sort = query.sort as string;
+  }
+
+  const sortQuery = filterQuery.sort(sort);
+
+  // Limit
+
+  let limit = 1;
+
+  if (query.limit) {
+    limit = query.limit as number;
+  }
+
+  const limitQuery = await sortQuery.limit(limit);
+
+  return limitQuery;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
