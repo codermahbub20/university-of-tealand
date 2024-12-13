ca0f2757c725249011ca1096e6142c3a89265c25
codermahbub20 <mkrefat5@gmail.com>
2024-12-13 20:43:17 +0600

    Simplified cast error

--- src/app/middlewares/globalErrorHandalers.ts
+++ src/app/middlewares/globalErrorHandalers.ts
@@ -8,6 +8,7 @@ import { TErrorSource } from '../interface/error';
 import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next): any => {
   let statusCode = err.statusCode || 500;
@@ -23,13 +24,20 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next): any => {
   // Create a middleware
 
   if (err instanceof ZodError) {
-    //
+    //zod error here
     const simplifiedError = handleZodError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
   } else if (err?.name === 'ValidationError') {
+    // Validation error Here
     const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    // Cast Error Here
+    const simplifiedError = handleCastError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
@@ -40,6 +48,7 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next): any => {
     message,
     errorSources,
     // error: err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
