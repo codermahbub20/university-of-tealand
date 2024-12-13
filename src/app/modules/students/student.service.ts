c828dc9416209485f2a6c41091beaa05b0bd7fa8
codermahbub20 <mkrefat5@gmail.com>
2024-12-13 23:47:35 +0600

    create pagination and field limiting

--- src/app/modules/students/student.service.ts
+++ src/app/modules/students/student.service.ts
@@ -25,7 +25,7 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   });
 
   // filtering
-  const excludedFields = ['searchTerm', 'sort', 'limit'];
+  const excludedFields = ['searchTerm', 'sort', 'limit', 'page', 'fields'];
 
   excludedFields.forEach((el) => delete queryObj[el]);
 
@@ -48,16 +48,34 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   const sortQuery = filterQuery.sort(sort);
 
   // Limit
-
+  let page = 1;
   let limit = 1;
+  let skip = 0;
 
   if (query.limit) {
-    limit = query.limit as number;
+    limit = Number(query.limit);
   }
 
-  const limitQuery = await sortQuery.limit(limit);
+  if (query.page) {
+    page = Number(query.page);
+    skip = (page - 1) * limit;
+  }
 
-  return limitQuery;
+  const paginateQuery = sortQuery.skip(skip);
+
+  const limitQuery = paginateQuery.limit(limit);
+
+  // field limiting
+
+  let fields = '__v';
+
+  if (query.fields) {
+    fields = (query.fields as string).split(',').join(' ');
+  }
+
+  const fieldQuery = await limitQuery.select(fields);
+
+  return fieldQuery;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
