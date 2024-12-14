3798ff368634fc50c9f2af8b081a4a550c5aa047
codermahbub20 <mkrefat5@gmail.com>
2024-12-14 15:42:58 +0600

    Course model created

---
+++ src/app/modules/Course/course.model.ts
@@ -0,0 +1,38 @@
+import { model, Schema } from 'mongoose';
+import { TCourse, TPreRequisite } from './course.interface';
+import { boolean } from 'zod';
+
+const preRequisiteSchema = new Schema<TPreRequisite>({
+  course: { type: Schema.Types.ObjectId },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const courseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  prefix: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  code: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  credits: {
+    type: Number,
+    trim: true,
+    required: true,
+  },
+  preRequisiteCourse: [preRequisiteSchema],
+});
+
+export const Course = model<TCourse>('Course', courseSchema);
