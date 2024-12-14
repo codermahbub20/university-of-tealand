ec58d47a3c983b118a1867450f2a92f846841a81
codermahbub20 <mkrefat5@gmail.com>
2024-12-14 15:32:00 +0600

    Course Interface Created

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,8 @@ import { userRoutes } from '../modules/user/user.routes';
 import { academicSemesterRoutes } from '../modules/academicSemestar/academicSemester.route';
 import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
+import { FacultyRoutes } from '../modules/faculty/faculty.route';
+import { AdminRoutes } from '../modules/Admin/admin.route';
 
 const router = Router();
 
@@ -15,6 +17,14 @@ const moduleRoutes = [
   {
     path: '/users',
     routes: userRoutes,
+  },
+  {
+    path: '/faculties',
+    route: FacultyRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
   },
   {
     path: '/academic-semesters',
@@ -30,6 +40,6 @@ const moduleRoutes = [
   },
 ];
 
-moduleRoutes.forEach((route) => router.use(route.path, route.routes));
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
