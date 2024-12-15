39436127e6f9ea05f12bc14938fe849892fbe3a8
codermahbub20 <mkrefat5@gmail.com>
2024-12-15 19:46:51 +0600

    Get all courses in to db

--- src/app/modules/Course/course.service.ts
+++ src/app/modules/Course/course.service.ts
@@ -1,10 +1,16 @@
 import { Course } from './course.model';
 
-const createCourse = async () => {
+const createCourseInToDB = async () => {
   const result = await Course.create();
   return result;
 };
 
+const getAllCoursesInToDB = async () => {
+  const result = await Course.find();
+  return result;
+};
+
 export const CourseServices = {
-  createCourse,
+  createCourseInToDB,
+  getAllCoursesInToDB,
 };
