742aa4ddee60a1ce5e980c9433748f9558c9afa6
codermahbub20 <mkrefat5@gmail.com>
2024-12-17 15:31:30 +0600

    Update validation schema created

--- src/app/modules/OfferedCourse/offeredCourse.validation.ts
+++ src/app/modules/OfferedCourse/offeredCourse.validation.ts
@@ -17,6 +17,17 @@ const createOfferedCourseValidationSchema = z.object({
   }),
 });
 
-const OfferedCourseValidation = {
+const updateOfferedCourseValidationSchema = z.object({
+  body: z.object({
+    faculty: z.string().optional(),
+    maxCapacity: z.number().optional(),
+    days: z.enum([...Days] as [string, ...string[]]).optional(),
+    startTime: z.string().optional(),
+    endTime: z.string().optional(),
+  }),
+});
+
+export const OfferedCourseValidation = {
   createOfferedCourseValidationSchema,
+  updateOfferedCourseValidationSchema,
 };
