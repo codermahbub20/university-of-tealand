930e42b6f3609e384c4438ea17d35bdd63ce7306
codermahbub20 <mkrefat5@gmail.com>
2024-12-17 15:23:28 +0600

    Offered course model created

--- src/app/modules/OfferedCourse/offeredCourse.interface.ts
+++ src/app/modules/OfferedCourse/offeredCourse.interface.ts
@@ -1,1 +1,17 @@
-export type Days = 'Sat' | 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri';
+import { Types } from 'mongoose';
+
+export type TDays = 'Sat' | 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri';
+
+export type TOfferedCourse = {
+  semesterRegistration: Types.ObjectId;
+  academicSemester: Types.ObjectId;
+  academicFaculty: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
+  course: Types.ObjectId;
+  faculty: Types.ObjectId;
+  maxCapacity: number;
+  section: number;
+  days: TDays;
+  startTime: string;
+  endTime: string;
+};
