abe5f07d86a96e7ae8d149137fa2bc4c10c62aac
codermahbub20 <mkrefat5@gmail.com>
2024-12-19 11:56:45 +0600

    Verify token using auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import CatchAsync from '../utils/CatchAsync';
 import AppError from '../errors/AppError';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return CatchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -9,6 +11,21 @@ const auth = () => {
     if (!token) {
       throw new AppError(401, 'You are not authorized user');
     }
+
+    // check if the token are valid
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        // err
+        if (err) {
+          throw new AppError(401, 'You are not authorized user');
+        }
+        // decoded undefined
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
   });
 };
 
